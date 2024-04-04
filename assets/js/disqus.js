dfb831fa6af978a36850ef8d1636ba9c5869544d
Chippalrus <chippalrus.dev@gmail.com>
2024-04-04 01:40:50 -0700

    a

--- assets/js/disqus.js
+++
@@ -1,1 +0,0 @@
-(function(){var d = document, s = d.createElement('script');s.src = 'https://chippalrus.disqus.com/embed.js';s.setAttribute('data-timestamp', +new Date());(d.head || d.body).appendChild(s);})();
