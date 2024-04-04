e1b00c4241900b3facb095d427aceaee8cb2396d
Chippalrus <chippalrus.dev@gmail.com>
2024-04-03 21:27:28 -0700

    a

---
+++ 도/js/disqus.js
@@ -0,0 +1,1 @@
+(function(){var d = document, s = d.createElement('script');s.src = 'https://chippalrus.disqus.com/embed.js';s.setAttribute('data-timestamp', +new Date());(d.head || d.body).appendChild(s);})();
